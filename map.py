5dcbe1a238493e74855b8a21755fbff740f28294
Matheo Letombe <matheo.letombe@etu.minesparis.psl.eu>
2026-01-30 10:16:41 +0100

    creation_map_2

--- map.py
+++ map.py
@@ -1,8 +1,26 @@
 # création de la map 
+import numpy as np 
+import random as rd 
+
+
 GRID_SIZE = 30
 INITIAL_SHEEP = 50
 INITIAL_WOLVES = 10
 INITIAL_GRASS_COVERAGE
 
 def create_map( GRID_SIZE, INITIAL_SHEEP, INITIAL_WOLVES, INITIAL_GRASS_COVERAGE):
+    map = np.zeros((GRID_SIZE, GRID_SIZE)) 
+    # Initialiser la couverture d'herbe
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE): 
+            r_grass = rd.random() 
+            r_sheep = rd.random()
+            r_wolf = rd.random()
+            if r_grass < INITIAL_GRASS_COVERAGE:
+                map[i][j] = (Grass(), Animal())
+            elif r_sheep < INITIAL_SHEEP / (GRID_SIZE * GRID_SIZE):
+                map[i][j] = (Grass(), Sheep())
+            elif r_wolf < INITIAL_WOLVES / (GRID_SIZE * GRID_SIZE):
+                map[i][j] = (Grass(), Wolf())
+            
 
